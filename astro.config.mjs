fdc57b6ffe1e9d12429f4863d72338437ae86d22
Naman Arora <namanarora1022@gmail.com>
2023-10-27 16:18:01 +0530

    feat: sanity astro setup done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
-
 import tailwind from '@astrojs/tailwind';
+import sanity from '@sanity/astro';
+import 'dotenv/config';
 
-// https://astro.build/config
 export default defineConfig({
     server: {
         port: 3000
     },
-    integrations: [solidJs(), tailwind()]
+    integrations: [
+        solidJs(),
+        tailwind(),
+        sanity({
+            projectId: process.env.VITE_SANITY_PROJECT_ID,
+            dataset: 'production',
+            apiVersion: '2022-08-10',
+            useCdn: true,
+            token: process.env.VITE_SANITY_TOKEN,
+            ignoreBrowserTokenWarning: true
+        })
+    ]
 });
--- typings/env.d.ts
+++ typings/env.d.ts
@@ -1,6 +1,12 @@
 declare global {
     namespace NodeJS {
-        interface ProcessEnv {}
+        interface ProcessEnv {
+            VITE_SANITY_TOKEN: string;
+            VITE_SANITY_PROJECT_ID: string;
+            VITE_EMAILJS_SERVICE_ID: string;
+            VITE_EMAILJS_TEMPLATE_ID: string;
+            VITE_EMAILJS_PUBLIC_KEY: string;
+        }
     }
 }
 
